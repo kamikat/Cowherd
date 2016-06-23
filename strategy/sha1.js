ace6161e8a81396af75db6ef8da0c98986095896
kirisetsz <kirisetsz@gmail.com>
2016-06-24 00:31:49 +0800

    add document comments

--- strategy/sha1.js
+++ strategy/sha1.js
@@ -1,4 +1,25 @@
-
+/**
+ * Use SHA-1 hash of the file as the storage key (SHA-1 hash is restored from qetag)
+ *
+ * @param body - `callbackBody` object
+ *
+ * default properties
+ * {
+ *   bucket: "$(bucket)",
+ *   key: "$(key)",
+ *   hash: "$(etag)",
+ *   etag: "$(etag)",
+ *   fname: "$(fname)",
+ *   fsize: "$(fsize)",
+ *   mimeType: "$(mimeType)",
+ *   uuid: "$(uuid)"
+ * }
+ *
+ * See http://developer.qiniu.com/article/kodo/kodo-developer/up/vars.html#magicvar
+ *
+ * Body object can be overrided `callbackBody` property in policy config
+ * to use custom variable or image/persistence related variables.
+ */
 module.exports = function (body) {
   var b64string = body.hash.replace('_', '/').replace('-', '+');
   var buf = new Buffer(b64string, 'base64');
